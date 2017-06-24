816eb0561e146d15ec41cf2aadc7dfdec21bac4f
Sadegh Hayeri <hayerisadegh@gmail.com>
2017-06-24 16:54:34 +0430

    Delete secrets.py

--- secrets.py
+++
@@ -1,9 +0,0 @@
-# Go to http://dev.twitter.com and create an app.
-# The consumer key and secret will be generated for you after
-consumer_key="ImmFONOvrN2FqxgXD0XU2aGMk"
-consumer_secret="nN7hfcbdPS5VRwZLpiyCbQB05yVbNJj1ege3n6Vbru0Lo9vRUY"
-
-# After the step above, you will be redirected to your app's page.
-# Create an access token under the the "Your access token" section
-access_token="749687324175376385-WJSDyr0rmgxOIISfVfoHCL0rq8rOKiE"
-access_token_secret="cSo2T7RLjPHBEAybgQUx8UnoEpAcsDqtWb2g4lOyVYF3N"
